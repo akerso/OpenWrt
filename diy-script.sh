6786180387ed316f4a8734b51e46b7bf2637767b
akerso <70606558+akerso@users.noreply.github.com>
2024-10-31 06:50:04 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -110,6 +110,9 @@ sed -i "s/${orig_version}/R${date_version} /g" package/lean/default-settings/fil
 # 修复 armv8 设备 xfsprogs 报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 
+# 修改
+#sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.10/g' target/linux/rockchip/Makefile
+
 # 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
