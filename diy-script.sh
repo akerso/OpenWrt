fcc4a3dcd1f2d338240d706673852e1638fa1f2d
akerso <70606558+akerso@users.noreply.github.com>
2025-01-10 00:38:52 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -48,6 +48,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # Themes
+git clone --depth=1 -b js https://github.com/0x676e67/luci-theme-design  package/luci-theme-design
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 # git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
